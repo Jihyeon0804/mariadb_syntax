7926f41f8514b6e2f7fcd3683f4191170f1ad74a
Jihyeon0804 <cam080421@naver.com>
2025-05-27 17:07:22 +0900

    SQL

--- 5.flow_statement.sql
+++ 5.flow_statement.sql
@@ -5,11 +5,11 @@ select id, if (name is null, '익명사용자', name) from author;
 -- ifnull (a, b) : a가 null이면 b를 반환, null이 아니면 a를 그대로 반환
 select id, ifnull(name, '익명사용자') from author;
 
--- case when :
+-- case when : case when 구문 끝에 end 써주어야 함
 select id,
- case
-  when name is null then '익명사용자'
-  when name = 'hong1' then '홍길동'
-  else name
- end
+    case
+        when name is null then '익명사용자'
+        when name = 'hong1' then '홍길동'
+        else name
+    end
 from author;
