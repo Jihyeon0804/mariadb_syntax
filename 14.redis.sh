bf5e7fe5c128d190307463cef5ba9e4e9ab7c164
Jihyeon0804 <cam080421@naver.com>
2025-06-04 09:04:16 +0900

    Redis

--- 14.redis.sh
+++ 14.redis.sh
@@ -14,8 +14,8 @@ select db번호
 # db내 모든 키 조회
 keys *
 
-# String 자료 구조 (가장 일반적)
 
+# String 자료 구조 (가장 일반적)
 # set을 통해 key:value 셋팅
 set key value
 set user1 hong1@naver.com
@@ -59,6 +59,7 @@ decr set stocks:prodcut:1
 # 위 데이터의 결과값을 spring서버를 통해 json 으로 변형항여, redis에 저장
 # 최종적인 데이터 형식 : {"name":"hong", "email":"hong@daum.net", "age":30}
 set member:info:1 "{\"name\":\"hong\", \"email\":\"hong@daum.net\", \"age\":30}" ex 1000
+
 
 # list 자료구조
 # redis의 list는 deque와 같은 자료 구조. 즉, double-endedd queue 자료 구조
@@ -106,6 +107,7 @@ rpush user:1:recent:product mango
 # 최근 본 상품 3개 조회
 lrange user:1:recent:product -3 -1
 
+
 # set 자료 구조 : 중복X, 순서X (좋아요, 조회 수 기능)
 sadd memberlist m1
 sadd memberlist m2
@@ -129,3 +131,46 @@ sismember posting:likes:1 a1@naver.com
 sadd posting:likes:1 a1@naver.com
 # 좋아요한 사람을 클릭하면
 sismember posting:likes:1
+
+
+# zset : sorted set (score 기준 정렬, 중복이라면 덮어쓰기 됨)
+# zset을 활용해서 최근 시간 순으로 정렬 가능
+# zset도 set이므로 같은 상품을 add할 경우에 중복이 제거되고, score(시간)값만 업데이트
+zadd user:1:recent:product 091330 mango
+zadd user:1:recent:product 091331 apple
+zadd user:1:recent:product 091332 banana
+zadd user:1:recent:product 091333 orange
+zadd user:1:recent:product 091334 apple
+
+# zset 조회 : zrange (zcore 기준 오름차순 정렬 조회), zrevrange (zcore 기준 내림차순 정렬 조회)
+zrange user:1:recent:product 0 2        # mango, banana, orange
+zrange user:1:recent:product -3 -1      # banana, orange, apple
+zrevrange user:1:recent:product 0 2     # apple, orange, banana
+# withscoress를 통해 score값까지 같이 출력
+zrevrange user:1:recent:product 0 2 withscores
+
+# 주식 시세 저장
+# 종목 : 삼성 전자, 시세 : 55,000원, 시간 : 현재 시간(유닉스 타임스탬프 -> 년월일시간을 초단위로 변환한 것)
+zadd stock:price:se 1748911141 55000
+zadd stock:price:lg 1748911141 100000
+zadd stock:price:se 1748911142 55000
+zadd stock:price:lg 1748911141 100000
+# 삼성전자의 현재 시세
+zrevrange stock:price:lg 0 0
+zrange stock:price:lg -1 -1
+
+
+# hashes : value가 map형태의 자료 구조 (key:value, key:value, ... 형태의 자료 구조)
+set member:info:1 "{\"name\":\"hong\", \"email\":\"hong@daum.net\", \"age\":30}"
+hset member:info:1 name hong email hong@daum.net age 30
+
+# 특정 값 조회
+hget member:info:1 name
+
+# 모든 객체 값 조회
+hgetall member:info:1
+
+# 특정 요소 값 수정
+hset member:info:1 name hong2
+
+# redis 활용 상황 : 빈번하고 변경되는 객체 값을 저장 시 효율적
